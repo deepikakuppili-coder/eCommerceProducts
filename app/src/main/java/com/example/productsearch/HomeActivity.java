de544c3da89bcdbeabeebb159a61c5e39e24edc2
deepikakuppili-coder <gn3ssankarcse@gmail.com>
2020-03-13 23:11:52 +0530

    search bar ccommit

--- app/src/main/java/com/example/productsearch/HomeActivity.java
+++ app/src/main/java/com/example/productsearch/HomeActivity.java
@@ -1,14 +1,83 @@
 package com.example.productsearch;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+
+     EditText searchEditText;
+    DatabaseReference databaseReference;
+    RecyclerView recyclerView;
+    ArrayList<String> nameList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        searchEditText=(EditText) findViewById(R.id.search_product);
+        recyclerView=(RecyclerView) findViewById(R.id.recycleView);
+
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,LinearLayoutManager.VERTICAL));
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                    if(!s.toString().isEmpty()){
+                        setAdapter(s.toString());
+                    }
+            }
+        });
+    }
+    private void setAdapter(String s){
+            databaseReference.child("products").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                        String pid=dataSnapshot1.getKey();
+                        String name=dataSnapshot1.child("products").getValue(String.class);
+                        if(name.contains(searchEditText.toString())){
+                            nameList.add(name);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
     }
 }
